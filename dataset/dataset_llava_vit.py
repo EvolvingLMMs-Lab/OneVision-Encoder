ccbd2102e1c310c33ff1b537cc1bde17a06e673d
Xiang An <anxiangsir@outlook.com>
2025-12-25 14:59:35 +0800

    updated

--- dataset/dataset_llava_vit.py
+++ dataset/dataset_llava_vit.py
@@ -10,96 +10,20 @@ from .properties import Property
 logger = logging.getLogger(__file__)
 
 list_coyo = [
-    "/vlm/data/coyo400m_part1/coyo700m_00",
-    "/vlm/data/coyo400m_part1/coyo700m_01",
-    "/vlm/data/coyo400m_part1/coyo700m_02",
-    "/vlm/data/coyo400m_part1/coyo700m_03",
-    "/vlm/data/coyo400m_part1/coyo700m_04",
-    "/vlm/data/coyo400m_part1/coyo700m_05",
-    "/vlm/data/coyo400m_part1/coyo700m_06",
-    "/vlm/data/coyo400m_part1/coyo700m_07",
-    "/vlm/data/coyo400m_part1/coyo700m_08",
-    "/vlm/data/coyo400m_part1/coyo700m_09",
-    "/vlm/data/coyo400m_part2/coyo700m_10",
-    "/vlm/data/coyo400m_part2/coyo700m_11",
-    "/vlm/data/coyo400m_part2/coyo700m_12",
-    "/vlm/data/coyo400m_part2/coyo700m_13",
-    "/vlm/data/coyo400m_part2/coyo700m_14",
-    "/vlm/data/coyo400m_part2/coyo700m_15",
-    "/vlm/data/coyo400m_part2/coyo700m_16",
-    "/vlm/data/coyo400m_part2/coyo700m_17",
-    "/vlm/data/coyo400m_part2/coyo700m_18",
-    "/vlm/data/coyo400m_part2/coyo700m_19",
-    "/vlm/data/coyo400m_part3/coyo700m_20",
-    "/vlm/data/coyo400m_part3/coyo700m_21",
-    "/vlm/data/coyo400m_part3/coyo700m_22",
-    "/vlm/data/coyo400m_part3/coyo700m_24",
-    "/vlm/data/coyo400m_part3/coyo700m_25",
-    "/vlm/data/coyo400m_part3/coyo700m_26",
-    "/vlm/data/coyo400m_part3/coyo700m_27",
-    "/vlm/data/coyo400m_part3/coyo700m_28",
-    "/vlm/data/coyo400m_part3/coyo700m_29",
-    "/vlm/data/coyo400m_part4/coyo700m_30",
-    "/vlm/data/coyo400m_part4/coyo700m_31",
-    "/vlm/data/coyo400m_part4/coyo700m_31",
+    ""
 ]
 list_laion = [
-    "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-20-tencentos",
-    "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-21-tencentos",
-    "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-23-tencentos",
-    "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-28-tencentos",
-    "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-34-tencentos",
-    "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-58-tencentos",
-    "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-62-tencentos",
-    "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-85-tencentos",
+    ""
 ]
 
 
 @DATASET_REGISTRY.register()
 def llava_vit_si_2025_12_12():
     list_coyo = [
-        "/vlm/data/coyo400m_part1/coyo700m_00",
-        "/vlm/data/coyo400m_part1/coyo700m_01",
-        "/vlm/data/coyo400m_part1/coyo700m_02",
-        "/vlm/data/coyo400m_part1/coyo700m_03",
-        "/vlm/data/coyo400m_part1/coyo700m_04",
-        "/vlm/data/coyo400m_part1/coyo700m_05",
-        "/vlm/data/coyo400m_part1/coyo700m_06",
-        "/vlm/data/coyo400m_part1/coyo700m_07",
-        "/vlm/data/coyo400m_part1/coyo700m_08",
-        "/vlm/data/coyo400m_part1/coyo700m_09",
-        "/vlm/data/coyo400m_part2/coyo700m_10",
-        "/vlm/data/coyo400m_part2/coyo700m_11",
-        "/vlm/data/coyo400m_part2/coyo700m_12",
-        "/vlm/data/coyo400m_part2/coyo700m_13",
-        "/vlm/data/coyo400m_part2/coyo700m_14",
-        "/vlm/data/coyo400m_part2/coyo700m_15",
-        "/vlm/data/coyo400m_part2/coyo700m_16",
-        "/vlm/data/coyo400m_part2/coyo700m_17",
-        "/vlm/data/coyo400m_part2/coyo700m_18",
-        "/vlm/data/coyo400m_part2/coyo700m_19",
-        "/vlm/data/coyo400m_part3/coyo700m_20",
-        "/vlm/data/coyo400m_part3/coyo700m_21",
-        "/vlm/data/coyo400m_part3/coyo700m_22",
-        "/vlm/data/coyo400m_part3/coyo700m_24",
-        "/vlm/data/coyo400m_part3/coyo700m_25",
-        "/vlm/data/coyo400m_part3/coyo700m_26",
-        "/vlm/data/coyo400m_part3/coyo700m_27",
-        "/vlm/data/coyo400m_part3/coyo700m_28",
-        "/vlm/data/coyo400m_part3/coyo700m_29",
-        "/vlm/data/coyo400m_part4/coyo700m_30",
-        "/vlm/data/coyo400m_part4/coyo700m_31",
-        "/vlm/data/coyo400m_part4/coyo700m_31",
+        ""
     ]
     list_laion = [
-        "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-20-tencentos",
-        "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-21-tencentos",
-        "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-23-tencentos",
-        "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-28-tencentos",
-        "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-34-tencentos",
-        "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-58-tencentos",
-        "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-62-tencentos",
-        "/vlm/data/LAION224M_HOI31M_IN13M_labeled_2024_03_05/LAION224M_HOI31M_IN13M_labeled_2024_03_05_VM-2-85-tencentos",
+        ""
     ]
     rank = int(os.getenv("RANK", "0"))
     world_size = int(os.getenv("WORLD_SIZE", "1")) # Expected to be 128
@@ -220,8 +144,7 @@ def llava_vit_si_ssd():
     world_size = int(os.getenv("WORLD_SIZE", "1"))
 
     patterns = [
-        "/data_*/mlcd/coyo700m*/*.rec",
-        "/data_*/mlcd/LAION224M_HOI31M_IN13M_labeled*/*.rec",
+        "*.rec"
     ]
 
     all_files = [f for pattern in patterns for f in glob.glob(pattern)]
@@ -376,125 +299,6 @@ def llava_vit_ocr_ssd():
         random_diff=100,
     )
 
-
-@DATASET_REGISTRY.register()
-def howto100m_kinetics_104948429_400000_split_80():
-    rank = int(os.getenv("RANK", "0"))
-    local_rank = int(os.getenv("LOCAL_RANK", "0"))
-    world_size = int(os.getenv("WORLD_SIZE", "1"))
-
-    assert world_size <= 80
-
-    list_mp4_label_path = f"/video_vit/dataset/configs_for_llava_vit_versions_0_0_0/preshuffled_trainset_split_80/preshuffled_trainset_part.lst_{rank:03d}"
-    # with open(list_mp4_label, "r", encoding="utf-8") as f:
-        # lines = f.readlines()
-
-    # lines = [x.strip().split(",")[0] for x in lines]
-    return Property(
-        name="howto100m_kinetics_104948429_400000_split_80",
-        prefixes=[list_mp4_label_path],
-        num_classes=400000,
-        num_examples=104948429 // world_size,
-        num_shards=1,
-        shard_id=0,
-        dali_type="decord",
-    )
-
-
-@DATASET_REGISTRY.register()
-def howto100m_kinetics_104948429_400000_split_128():
-    rank = int(os.getenv("RANK", "0"))
-    local_rank = int(os.getenv("LOCAL_RANK", "0"))
-    world_size = int(os.getenv("WORLD_SIZE", "1"))
-
-    assert world_size <= 128
-
-    list_mp4_label_path = f"/video_vit/dataset/configs_for_llava_vit_versions_0_0_0/preshuffled_trainset_split_128/preshuffled_trainset_part.lst_{rank:03d}"
-    # with open(list_mp4_label, "r", encoding="utf-8") as f:
-        # lines = f.readlines()
-
-    # lines = [x.strip().split(",")[0] for x in lines]
-    return Property(
-        name="howto100m_kinetics_104948429_400000_split_128",
-        prefixes=[list_mp4_label_path],
-        num_classes=400000,
-        num_examples=104948429 // world_size,
-        num_shards=1,
-        shard_id=0,
-        dali_type="decord",
-    )
-
-@DATASET_REGISTRY.register()
-def howto100m_panda70m_kinetics_126409811_400000_split_128():
-    rank = int(os.getenv("RANK", "0"))
-    local_rank = int(os.getenv("LOCAL_RANK", "0"))
-    world_size = int(os.getenv("WORLD_SIZE", "1"))
-
-    assert world_size <= 128
-
-    list_mp4_label_path = f"/video_vit/dataset/configs_for_llava_vit_versions_0_0_1_add_pandas70M/train_how_to_100m_panda70m_k710_split_128/part_{rank:03d}"
-    # with open(list_mp4_label, "r", encoding="utf-8") as f:
-        # lines = f.readlines()
-
-    # lines = [x.strip().split(",")[0] for x in lines]
-    return Property(
-        name="howto100m_panda70m_kinetics_126409811_400000_split_128",
-        prefixes=[list_mp4_label_path],
-        num_classes=400000,
-        num_examples=104948429 // world_size,
-        num_shards=1,
-        shard_id=0,
-        dali_type="decord",
-    )
-
-
-@DATASET_REGISTRY.register()
-def configs_for_llava_vit_versions_0_0_2_add_pandas70M():
-    rank = int(os.getenv("RANK", "0"))
-    local_rank = int(os.getenv("LOCAL_RANK", "0"))
-    world_size = int(os.getenv("WORLD_SIZE", "1"))
-
-    assert world_size <= 128
-
-    list_mp4_label_path = f"/video_vit/dataset/configs_for_llava_vit_versions_0_0_2_add_pandas70M/train_how_to_100m_panda70m_k710_split_128/part_{rank:03d}"
-    # with open(list_mp4_label, "r", encoding="utf-8") as f:
-        # lines = f.readlines()
-
-    # lines = [x.strip().split(",")[0] for x in lines]
-    return Property(
-        name="configs_for_llava_vit_versions_0_0_2_add_pandas70M",
-        prefixes=[list_mp4_label_path],
-        num_classes=400000,
-        num_examples=116693632 // world_size,
-        num_shards=1,
-        shard_id=0,
-        dali_type="decord",
-    )
-
-
-@DATASET_REGISTRY.register()
-def configs_for_llava_vit_versions_0_0_2_add_pandas70M_filtered():
-    rank = int(os.getenv("RANK", "0"))
-    local_rank = int(os.getenv("LOCAL_RANK", "0"))
-    world_size = int(os.getenv("WORLD_SIZE", "1"))
-
-    assert world_size <= 128
-
-    list_mp4_label_path = f"/video_vit/dataset/configs_for_llava_vit_versions_0_0_2_add_pandas70M/train_how_to_100m_panda70m_k710_filtered_split_128/part_{rank:03d}"
-    # with open(list_mp4_label, "r", encoding="utf-8") as f:
-        # lines = f.readlines()
-
-    # lines = [x.strip().split(",")[0] for x in lines]
-    return Property(
-        name="configs_for_llava_vit_versions_0_0_2_add_pandas70M_filtered",
-        prefixes=[list_mp4_label_path],
-        num_classes=400000,
-        num_examples=1269187 * 128,
-        num_shards=1,
-        shard_id=0,
-        dali_type="decord",
-    )
-
 @DATASET_REGISTRY.register()
 def configs_for_llava_vit_versions_0_0_2_square_with_index_filtered():
     rank = int(os.getenv("RANK", "0"))
@@ -502,7 +306,7 @@ def configs_for_llava_vit_versions_0_0_2_square_with_index_filtered():
     world_size = int(os.getenv("WORLD_SIZE", "1"))
 
     assert world_size <= 128
-    list_mp4_label_path = f"/video_vit/dataset/configs_for_llava_vit_versions_0_0_2_add_pandas70M/train_how_to_100m_panda70m_k710_square_with_index_filtered_split_128/part_{rank:03d}"
+    list_mp4_label_path = f"part_{rank:03d}"
 
     return Property(
         name="configs_for_llava_vit_versions_0_0_2_square_with_index_filtered",
@@ -515,28 +319,6 @@ def configs_for_llava_vit_versions_0_0_2_square_with_index_filtered():
     )
 
 
-@DATASET_REGISTRY.register()
-def configs_for_llava_vit_versions_0_0_2_add_pandas70M_split_80():
-    rank = int(os.getenv("RANK", "0"))
-    local_rank = int(os.getenv("LOCAL_RANK", "0"))
-    world_size = int(os.getenv("WORLD_SIZE", "1"))
-
-    assert world_size <= 80
-
-    list_mp4_label_path = f"/video_vit/dataset/configs_for_llava_vit_versions_0_0_2_add_pandas70M/train_how_to_100m_panda70m_k710_split_80/part_{rank:03d}"
-    # with open(list_mp4_label, "r", encoding="utf-8") as f:
-        # lines = f.readlines()
-
-    # lines = [x.strip().split(",")[0] for x in lines]
-    return Property(
-        name="configs_for_llava_vit_versions_0_0_2_add_pandas70M",
-        prefixes=[list_mp4_label_path],
-        num_classes=400000,
-        num_examples=116693632 // world_size,
-        num_shards=1,
-        shard_id=0,
-        dali_type="decord",
-    )
 
 @DATASET_REGISTRY.register()
 def fake_data():
