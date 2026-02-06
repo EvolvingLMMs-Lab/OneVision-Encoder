94ae0cc92486d39ee2c64f5701269a0b29bd396c
yiyexy <yiye.xieyin@gmail.com>
2026-02-06 11:59:34 +0800

    del some useless code

--- llava_next/llava/model/multimodal_encoder/builder.py
+++ llava_next/llava/model/multimodal_encoder/builder.py
@@ -1,6 +1,5 @@
 from .siglip2_naflex import SigLip2NaflexVisionTower
 from .onevision_encoder import OneVisionEncoderTower
-from .qwen3_vl_encoder import Qwen3VLVisionTower
 
 
 def build_vision_tower(vision_tower_cfg, **kwargs):
@@ -11,8 +10,5 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
 
     if "onevision" in vision_tower.lower():
         return OneVisionEncoderTower(vision_tower, vision_tower_cfg=vision_tower_cfg, **kwargs)
-
-    if "qwen3" in vision_tower.lower():
-        return Qwen3VLVisionTower(vision_tower, vision_tower_cfg=vision_tower_cfg, **kwargs)
 
     raise ValueError(f"Unknown vision tower: {vision_tower}")
